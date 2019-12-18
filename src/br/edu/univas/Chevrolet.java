c6b8b7344f8f2e6d1a30da1d80cc48f20dd89ec5
Wellington <wellingtonsasori@hotmail.com>
2019-12-17 21:28:26 -0300

    primeiro comit

---
+++ src/br/edu/univas/Chevrolet.java
@@ -0,0 +1,16 @@
+package br.edu.univas;
+
+public class Chevrolet extends Carro{
+	
+	public Chevrolet(String m)
+    {
+          nomeModelo = m;
+    }
+
+    @Override
+    public Carro clone() throws CloneNotSupportedException
+    {
+          return (Chevrolet)super.clone();
+    }
+
+}
---
+++ src/br/edu/univas/Ford.java
@@ -0,0 +1,16 @@
+package br.edu.univas;
+
+public class Ford extends Carro{
+	
+	public Ford(String m)
+    {
+          nomeModelo = m;
+    }
+
+    @Override
+    public Carro clone() throws CloneNotSupportedException
+    {
+          return (Ford)super.clone();
+    }
+
+}
